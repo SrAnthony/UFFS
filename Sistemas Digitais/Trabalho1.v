da87b94a4c34bb32e04c147977d46eb19b00b7bd
SrAnthony <anthony.nadaletti@gmail.com>
2017-08-29 12:09:20 -0300

    Enviado primeiro trabalho

---
+++ Sistemas Digitais/Trabalho1.v
@@ -0,0 +1,20 @@
+module piscaleds1(
+	input CLOCK_50, 
+	input [3:0] KEY, 
+	input [9:0] SW, 
+	output [7:0] LEDG, 
+	output [7:0] LEDR
+);
+
+	reg [25:0] cont = 0;
+	reg l = 0;
+	assign LEDR = 1;
+
+	always @(posedge CLOCK_50) begin
+		cont = cont + 1;
+		if(cont == 0)
+			l = ~l;
+	end
+
+
+endmodule
