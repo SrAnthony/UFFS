c29e6c15a178f5c785d7a7dc60d73fde447a40a6
SrAnthony <anthony.nadaletti@gmail.com>
2017-08-29 19:28:50 -0300

    Update tr2

--- Estrutura de Dados I/ex002.c
+++ Estrutura de Dados I/ex002.c
@@ -1,3 +1,9 @@
+/*
+*  ============================
+*  Anthony Nadaletti
+*  https://github.com/SrAnthony
+*  ============================
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdio_ext.h>
