b075ab291547681660d3dc35a9d76cc369f3a361
SrAnthony <anthony.nadaletti@gmail.com>
2017-09-05 15:15:27 -0300

    Eu to perdido

--- Sistemas Digitais/VGA/vga.v
+++ Sistemas Digitais/VGA/vga.v
@@ -1,13 +1,13 @@
-module vga(
-	input CLOCK_50,
-	output [3:0] VGA_R,
-	output [3:0] VGA_G,
+module vga (
+  	input CLOCK_50,
+  	output [3:0] VGA_R,
+  	output [3:0] VGA_G,
 	output [3:0] VGA_B,
-	output VGA_HS,
+ 	output VGA_HS,
 	output VGA_VS);
 
 reg [10:0] cx = 0;
-reg [9:0] cy = 0;
+reg [9:0]  cy = 0;
 
 assign VGA_R = v ? 4'hf : 4'b0;
 assign VGA_G = v ? 4'hf : 4'b0;
@@ -19,18 +19,21 @@ assign VGA_HS = cx >= 190;
 assign VGA_VS = cy >= 2;
 
 always @(posedge CLOCK_50) begin
-	if(cx == 1585) begin
-		if(cy == 525) begin
+	if (cx == 1585) 
+    begin
+        if (cy == 525) begin
 			cy <= 0;
-		end
-		else
+        end
+		else 
 			cy <= cy + 1;
 		cx <= 0;
 	end
-	else begin
-		//cy <= cy;
+    else 
+    begin
+        // cy <= cy;
 		cx <= cx + 1;
-	end
+    end
 end
+
 
 endmodule
