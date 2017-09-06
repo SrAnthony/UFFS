dd5eadc28bfe31da531774c5c491d53e1973763e
SrAnthony <anthony.nadaletti@gmail.com>
2017-09-05 21:16:33 -0300

    Erro de interpretação do exercício? Talvez.

--- Programação I/Lista I/ex002.java
+++ Programação I/Lista I/ex002.java
@@ -6,7 +6,9 @@
 */
 class ex002{
 	public static void main(String[] args){
-		for(int i=0; i < 500; )
-			System.out.println(++i);
+		int soma = 0;
+		for(int i=1; i <= 500; i++)
+			soma+=i;
+		System.out.println(soma);
 	}
 }
